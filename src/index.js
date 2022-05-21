d3f0d53182984ee65f88b2a21effd550d8243e74
Keyvan Alonso <keyvanalonso@gmail.com>
2022-05-21 14:55:04 +0200

    Clear more info and clear table functionality

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ const artCollection = []
 let tableLen = 0
 
 window.addEventListener('DOMContentLoaded', () => {
-    getInitialIDs()    
+    getInitialIDs()
+    clearMoreInfoInit()
+    clearTableInit()
 })
 
-function getInitialIDs(){
+function getInitialIDs() {
     fetch(BASE_URL + '/search?isHighlight=true&isOnView=true|q=*')
-        .then(function (resp){
+        .then(function (resp) {
             return resp.json()
         })
-        .then (data => {
+        .then(data => {
             let idList = Object.values(data.objectIDs)
             makeCards(idList)
             const loadMore = document.getElementById('loadNewCards')
@@ -21,19 +23,19 @@ function getInitialIDs(){
         })
 }
 
-function makeCards(idList){
+function makeCards(idList) {
 
-    for(let i = 0; i < CALLCOUNT; i++){
+    for (let i = 0; i < CALLCOUNT; i++) {
         let randIndex = Math.floor(Math.random() * (idList.length))
         fetch(BASE_URL + `/objects/${idList[randIndex]}`)
-        .then(function (resp){
-            return resp.json()
-        })
-        .then (data => {
-            if(data['primaryImageSmall'] != "" && data['GalleryNumber'] != ""){
-                cardHTMLer(data)
-            }
-        })
+            .then(function (resp) {
+                return resp.json()
+            })
+            .then(data => {
+                if (data['primaryImageSmall'] != "" && data['GalleryNumber'] != "") {
+                    cardHTMLer(data)
+                }
+            })
     }
 }
 
@@ -46,12 +48,12 @@ function cardHTMLer(data) {
     newCard.id = (`${data['objectID']}card`)
 
     const newH2 = document.createElement('h2')
-    if(data['title'].length > 20){
-        newH2.innerText = `${data['title'].slice(0,18)}...`
+    if (data['title'].length > 20) {
+        newH2.innerText = `${data['title'].slice(0, 18)}...`
     } else {
         newH2.innerText = `${data['title']}`
     }
-    
+
     const newImg = document.createElement('img')
     newImg.src = `${data['primaryImageSmall']}`
     newImg.className = "artImgSmall"
@@ -65,7 +67,7 @@ function cardHTMLer(data) {
     newLikeBtn.id = `${data['objectID']}`
     newLikeBtn.innerText = '✔️'
     newLikeBtn.addEventListener('click', (e) => liker(e))
-    
+
     const newDislikeBtn = document.createElement('button')
     newDislikeBtn.className = "dislike-btn"
     newDislikeBtn.id = `${data['title']}dislike`
@@ -74,30 +76,30 @@ function cardHTMLer(data) {
 
     newBtnDiv.append(newLikeBtn, newDislikeBtn)
     newCard.append(newH2, newImg, newBtnDiv)
-    
+
     artDiv.appendChild(newCard)
 }
 
-function liker(event){
+function liker(event) {
     event.preventDefault()
     const cardGroup = document.getElementsByClassName('card')
-    for (card of cardGroup){
+    for (card of cardGroup) {
         card.style.background = "#e4e4e4"
     }
     const topText = document.getElementById('introTxt')
     topText.innerText = 'Below is a summary of the works that you have liked! You can also click on the images in the table to open objects page on the MET website.'
     const artid = event.target.id
-    
+
     tabler(artid)
     cardCloser(artid)
 }
 
-function dispMoreInfo(artid){
+function dispMoreInfo(artid) {
     const introText = document.getElementById('introTxt')
     introText.innerText = ''
-    
+
     const cardGroup = document.getElementsByClassName('card')
-    for (card of cardGroup){
+    for (card of cardGroup) {
         card.style.background = "#e4e4e4"
     }
 
@@ -117,15 +119,15 @@ function dispMoreInfo(artid){
     }
 }
 
-function cardCloser(artid){
+function cardCloser(artid) {
     const dislikedCard = document.getElementById(`${artid}card`)
     dislikedCard.remove()
 }
 
-function tabler(artid){
+function tabler(artid) {
     const tableDiv = document.getElementById('tableDiv')
-    
-    if (tableLen == 0){
+
+    if (tableLen == 0) {
         tableDiv.innerHTML = (`<table id='table'><tr><th>Image</td><th>Info</td></tr></table>`)
         tableLen++
     }
@@ -139,7 +141,7 @@ function tabler(artid){
                 newRow.innerHTML = (`
             <td><a href="https://www.metmuseum.org/art/collection/search/${artid}" target="_blank"><img class="tableImage" src="${artCollection[art]['primaryImageSmall']}" alt="${artCollection[art]['title']}"></a></td><td>Title: ${artCollection[art]['title']}
             <br><br>
-            Gallery: ${artCollection[art]['GalleryNumber']}<br>NOTE: This item is located the the MET Cloisters, a ~40 minute trip on public transit.</td>`)
+            Gallery: ${artCollection[art]['GalleryNumber']}<br>NOTE: This item is located the the MET Cloisters, a ~40 minute trip on public transit from the MET's Upper East Side Location.</td>`)
             } else {
                 newRow.innerHTML = (`
             <td><a href="https://www.metmuseum.org/art/collection/search/${artid}" target="_blank"><img class="tableImage" src="${artCollection[art]['primaryImageSmall']}" alt="${artCollection[art]['title']}"></a></td><td>Title: ${artCollection[art]['title']}
@@ -149,4 +151,18 @@ function tabler(artid){
             artTable.appendChild(newRow)
         }
     }
+}
+
+function clearMoreInfoInit() {
+    const moreInfo = document.getElementById('clearMoreInfo')
+    const introTxt = document.getElementById('introTxt')
+    moreInfo.addEventListener('click', (e) => introTxt.innerHTML = '')
+}
+
+function clearTableInit() {
+    const clearTable = document.getElementById('clearTable')
+    const tableDiv = document.getElementById('tableDiv')
+    clearTable.addEventListener('click', (e) => {
+        tableDiv.innerHTML = (`<table id='table'><tr><th>Image</td><th>Info</td></tr></table>`)
+    })
 }
